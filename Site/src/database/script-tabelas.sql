35a5ee7b3a68313981c18ed2169dc6c26998a585
twitterpontocom <luca.pereira@sptech.school>
2025-06-02 07:59:22 -0300

    CSS da página de login e cadastro em 50%

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE easyfiction;
 USE easyfiction;
 
+-- Tabela para registrar as fotos de perfil da tela inicial
 CREATE TABLE fotos_perfil (
 id INT PRIMARY KEY AUTO_INCREMENT,
 personagem VARCHAR (45),
-artista VARCHAR (45),
-caminho_arquivo VARCHAR (100)
+artista VARCHAR (45)
 );
 
+-- Tabela para registrar os usuários
 CREATE TABLE usuarios (
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR (45),
@@ -19,15 +20,19 @@ fk_fotos_perfil INT,
 	FOREIGN KEY (fk_fotos_perfil) REFERENCES fotos_perfil (id)
 );
 
+-- Tabela para registrar os quadrinhos
 CREATE TABLE titulos (
 id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR (100),
 roteirista VARCHAR (50),
 desenhista VARCHAR (50),
-descricao VARCHAR (500),
-editora VARCHAR (45)
+descricao VARCHAR (1000),
+editora VARCHAR (45),
+numero_edicao INT,
+total_paginas INT
 );
 
+-- Tabela para registrar atividade do usuário
 CREATE TABLE atividades (
 id INT AUTO_INCREMENT,
 timestamp_inicio DATETIME default CURRENT_TIMESTAMP,
@@ -40,13 +45,34 @@ fk_titulos INT,
     FOREIGN KEY (fk_titulos) REFERENCES titulos(id)
 );
 
+-- Inserção das fotos de perfil no banco de dados
 INSERT INTO fotos_perfil VALUES
-	(default, 'Batman', 'Dan Mora', '\assets\fotos_perfil\id1 - Batman.png'),
-    (default, 'Supergirl', 'Bilquis Evely', '\assets\fotos_perfil\id2 - Supergirl.png'),
-    (default, 'Godspeed', 'Carmine Di Giandomenico', '\assets\fotos_perfil\id3 - Godspeed.png'),
-    (default, 'Homem-Aranha', 'Ryan Ottley', '\assets\fotos_perfil\id4 - Homem-Aranha.png'),
-    (default, 'Tempestade', 'Lucas Werneck', '\assets\fotos_perfil\id5 - Tempestade.png'),
-    (default, 'Galactus', 'Leonard Kirk', '\assets\fotos_perfil\id6 - Galactus.png'),
-    (default, 'Optimus Prime', 'Daniel Warren Johnson', '\assets\fotos_perfil\id7 - Optimus Prime.png'),
-    (default, 'Elita One', 'Jorge Corona', '\assets\fotos_perfil\id8 - Elita One.png'),
-    (default, 'Megatron', 'Jorge Corona', '\assets\fotos_perfil\id9 - Megatron.png');
+	(default, 'Batman', 'Dan Mora'),
+    (default, 'Supergirl', 'Bilquis Evely'),
+    (default, 'Godspeed', 'Carmine Di Giandomenico'),
+    (default, 'Homem-Aranha', 'Ryan Ottley'),
+    (default, 'Tempestade', 'Lucas Werneck'),
+    (default, 'Galactus', 'Leonard Kirk'),
+    (default, 'Optimus Prime', 'Daniel Warren Johnson'),
+    (default, 'Elita One', 'Jorge Corona'),
+    (default, 'Megatron', 'Jorge Corona');
+
+-- Inserção dos quadrinhos no banco de dados
+INSERT INTO titulos VALUES
+	(default, 'Supergirl - Mulher do Amanhã', 'Tom King', 'Bilquis Evely',
+		'Kara Zor-El passou por muitas aventuras épicas ao longo dos anos, mas hoje acredita estar sem propósito. Aonde quer que vá, as pessoas só a veem como prima do Superman. Até que tudo muda, quando uma garota alienígena a procura para uma missão de vingança contra os vilões que exterminaram seu planeta. Agora, uma kryptoniana, um cachorro e uma criança com o coração partido partem para o espaço em uma jornada que mudará suas vidas para sempre. Escrita pelo aclamado Tom King e com arte da brilhante Bilquis Evely, a obra foi indicada ao Prêmio Eisner de melhor minissérie.',
+        'DC Comics', 1, 25),
+
+	(default, 'Transformers', 'Daniel Warren Johnson', 'Daniel Warren Johnson',
+		'Optimus Prime deveria ter liderado os Autobots à vitória. Em vez disso, o destino de Cybertron é desconhecidoe seus aliados caíram longe de casa, ao lado de seus inimigos: os Decepticons. À medida que essas forças titânicas renovam sua guerra na Terra, uma coisa fica imediatamente clara: o planeta nunca mais será o mesmo. Novas alianças são formadas, o campo de batalha se transforma e a única esperança de sobrevivência da humanidade é Optimus Prime.',
+        'Skybound', 1, 33),
+        
+	(default, 'Tempestade', 'Murewa Ayodele', 'Lucas Werneck',
+		'Ororo Munroe viveu muitas vidas. Ela foi uma ladra, uma deusa, uma X-Men, uma rainha e agora... uma Vingadora! Ela é um mutante nível omega, sendo a mais respeitada e influente porta voz mutante no cenário mundial, ela pretende ser uma força de mudança positiva. Porém um grande colapso em uma instalação nuclear na cidade de Oklahoma tira Tempestade de seu santuário em Atlanta e a leva para um conflito moral que testará sua determinação!',
+        'Marvel', 1, 29);
+DROP TABLES titulos;
+-- Marcando um confere
+SELECT * FROM fotos_perfil;
+SELECT * FROM titulos;
+SELECT * FROM usuarios;
+SELECT * FROM atividades;
